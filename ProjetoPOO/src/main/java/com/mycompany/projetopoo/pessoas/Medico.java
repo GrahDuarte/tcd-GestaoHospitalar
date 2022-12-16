9e25c50078c401f1c68cc8aefddade980025ed63
Suellen <103966382+SuKarolainy@users.noreply.github.com>
2022-12-15 21:32:42 -0300

    Add files via upload

---
+++ ProjetoPOO/src/main/java/com/mycompany/projetopoo/pessoas/Medico.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projetopoo.pessoas;
+
+import com.mycompany.projetopoo.dados.Pessoa;
+
+/**
+ *
+ * @author suKarolainy
+ */
+public class Medico extends Pessoa{
+    private String crm;
+    private String especialidade;
+
+    //<editor-fold defaultstate="collapsed" desc="Constructors">
+    public Medico() {
+    }
+
+    public Medico(String crm, String especialidade) {
+        this.crm = crm;
+        this.especialidade = especialidade;
+    }
+    //</editor-fold>
+    
+    
+    
+    //<editor-fold defaultstate="collapsed" desc="Getters/Setters">
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+    //</editor-fold>
+    
+    @Override
+    public String toString() {
+        return "Medico{" 
+                + "crm=" + crm 
+                + ", especialidade=" + especialidade 
+                + '}';
+    }
+}
